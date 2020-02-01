387c29b53c3b3068d2056d7bc33b0f49da519f69
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2020-02-01 10:25:31 +0530

    added more tests to check handlers.js

--- test/handlersTest.js
+++ test/handlersTest.js
@@ -17,6 +17,58 @@ describe('GET nonExisting Url', () => {
       .get('/badPage')
       .set('Accept', '*/*')
       .expect(404)
-      .expect('Content-Length', '9', done);
+      .expect('Content-Length', '9', done)
+      .expect(/Not Found/);  
+  });
+});
+
+describe('GET Abeliophyllum page', () => {
+  it('should return abeliophyllum page', (done) => {
+    request(app.serve.bind(app))
+      .get('/abeliophyllum.html')
+      .set('Accept', '*/*')
+      .expect(200)
+      .expect('Content-Type', 'text/html', done);
+  });
+});
+
+describe('GET Ageratum page', () => {
+  it('should return Ageratum page', (done) => {
+    request(app.serve.bind(app))
+      .get('/Ageratum.html')
+      .set('Accept', '*/*')
+      .expect(200)
+      .expect('Content-Type', 'text/html', done);
+  });
+});
+
+describe('GET GuestBook page', () => {
+  it('should return GuestBook page', (done) => {
+    request(app.serve.bind(app))
+      .get('/guestBook.html')
+      .set('Accept', '*/*')
+      .expect(200)
+      .expect('Content-Type', 'text/html', done);
+  });
+});
+
+describe('GET hideCanForSec script', () => {
+  it('should return hideCanForSec script', (done) => {
+    request(app.serve.bind(app))
+      .get('/scripts/hideCanForSec.js')
+      .set('Accept', '*/*')
+      .expect(200)
+      .expect('Content-Type', 'application/javascript', done)
+      .expect(/hideCanForSec = function()/);
+  });
+});
+
+describe('GET a css file', () => {
+  it('should return style.css script', (done) => {
+    request(app.serve.bind(app))
+      .get('/css/style.css')
+      .set('Accept', '*/*')
+      .expect(200)
+      .expect('Content-Type', 'text/css', done);
   });
 });
